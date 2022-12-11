c2a83c7fe640987a1af1dbdca027dda5549da761
Max <maxvhanamane@gmail.com>
2022-12-11 21:39:51 +0530

    testing....

--- src/index.ts
+++ src/index.ts
@@ -102,7 +102,7 @@ app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }))
 app.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/login', session: true }),
   function (req, res) {
-    res.redirect('http://localhost:5173');
+    res.redirect('https://decker.vercel.app');
   });
 
 
